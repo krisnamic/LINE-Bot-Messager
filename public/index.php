143477f3c2383aa869bd7d4118e73b5aec63e717
Michael K <cahyadi.michael.mkc24@gmail.com>
2021-01-05 07:57:36 +0700

    implementasi reply message

---
+++ public/index.php
@@ -0,0 +1,87 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+ 
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+ 
+use \LINE\LINEBot;
+use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use \LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\AudioMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\VideoMessageBuilder;
+use \LINE\LINEBot\SignatureValidator as SignatureValidator;
+ 
+$pass_signature = true;
+ 
+// set LINE channel_access_token and channel_secret
+$channel_access_token = "ux2r+507FdAh17ZOuzppu4U+OLQJTRe6lzJhi6M92218D72VcHCX2TerJKUDEA+rz6hizCE2Hl5qfTN0+ewJYG8BjljrIxyR4ig2as3bh23nn03C20pxLykzsdRHJhKHeFvgjgq1cjuWYNXav2OzrgdB04t89/1O/w1cDnyilFU=";
+$channel_secret = "7801f8077daf4e86014a2c4aa6d69127";
+ 
+// inisiasi objek bot
+$httpClient = new CurlHTTPClient($channel_access_token);
+$bot = new LINEBot($httpClient, ['channelSecret' => $channel_secret]);
+ 
+$app = AppFactory::create();
+$app->setBasePath("/public");
+ 
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello World!");
+    return $response;
+});
+ 
+// buat route untuk webhook
+$app->post('/webhook', function (Request $request, Response $response) use ($channel_secret, $bot, $httpClient, $pass_signature) {
+    // get request body and line signature header
+    $body = $request->getBody();
+    $signature = $request->getHeaderLine('HTTP_X_LINE_SIGNATURE');
+ 
+    // log body and signature
+    file_put_contents('php://stderr', 'Body: ' . $body);
+ 
+    if ($pass_signature === false) {
+        // is LINE_SIGNATURE exists in request header?
+        if (empty($signature)) {
+            return $response->withStatus(400, 'Signature not set');
+        }
+ 
+        // is this request comes from LINE?
+        if (!SignatureValidator::validateSignature($body, $channel_secret, $signature)) {
+            return $response->withStatus(400, 'Invalid signature');
+        }
+    }
+    
+    // kode aplikasi nanti disini
+    $data = json_decode($body, true);
+    if(is_array($data['events'])){
+        foreach ($data['events'] as $event)
+        {
+            if ($event['type'] == 'message')
+            {
+                if($event['message']['type'] == 'text')
+                {
+                    // send same message as reply to user
+                    $result = $bot->replyText($event['replyToken'], $event['message']['text']);
+    
+    
+                    // or we can use replyMessage() instead to send reply message
+                    // $textMessageBuilder = new TextMessageBuilder($event['message']['text']);
+                    // $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
+    
+    
+                    $response->getBody()->write(json_encode($result->getJSONDecodedBody()));
+                    return $response
+                        ->withHeader('Content-Type', 'application/json')
+                        ->withStatus($result->getHTTPStatus());
+                }
+            }
+        }
+        return $response->withStatus(200, 'for Webhook!'); //buat ngasih response 200 ke pas verify webhook
+    }
+    return $response->withStatus(400, 'No event sent!');
+});
+$app->run();
+ 
