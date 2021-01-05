2e6ff1f0ce5a0eba1b43f1d4096464761db17859
Michael K <cahyadi.michael.mkc24@gmail.com>
2021-01-05 11:12:20 +0700

    implementasi content 36

--- public/index.php
+++ public/index.php
@@ -83,15 +83,18 @@ $app->post('/webhook', function (Request $request, Response $response) use ($cha
                         ]);
 
                     } 
-                    elseif (strtolower($event['message']['text']) == 'buka menu') {
-                        $result = $bot->replyMessage($event['replyToken'], "Silakan lik link di bawah ini:\n\nhttps://liff.line.me/1655338109-RpZmDMGN");
+                    elseif (strtolower($event['message']['text']) == 'pesan sekarang') {
+                        $result = $bot->replyMessage($event['replyToken'], "Silakan klik link di bawah ini:\n\nhttps://liff.line.me/1655338109-RpZmDMGN");
+                    } 
+                    elseif (strtolower($event['message']['text']) == 'help') {
+                        $result = $bot->replyMessage($event['replyToken'], "Halo selamat datang di Virtual Restaurant, " . $profile['displayName'] . "\n\nKetik keyword di bawah ini untuk menggunakan aplikasi Virtual Restaurant:\n\n1. Buka menu\n2. Pesan sekarang\n3.Help");
                     } 
                     else {
                         // send same message as reply to user
                         $userId = $event['source']['userId'];
                         $getprofile = $bot->getProfile($userId);
                         $profile = $getprofile->getJSONDecodedBody();
-                        $greetings = new TextMessageBuilder("Halo selamat datang di Virtual Restaurant, " . $profile['displayName'] . "\n\nKetik keyword di bawah ini untuk menggunakan aplikasi Virtual Restaurant:\n\n1. Buka menu\n2. Pesan sekarang");
+                        $greetings = new TextMessageBuilder("Keyword yang anda masukkan tidak tersedia, silakan ketik 'Help' untuk bantuan.");
 
                         $result = $bot->replyMessage($event['replyToken'], $greetings);
                         $response->getBody()->write(json_encode($result->getJSONDecodedBody()));
